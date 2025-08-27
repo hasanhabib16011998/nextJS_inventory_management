e6af730ba2186b9f13cfeff765a0118e3e7cb499
hasanhabib899161 <hasan16habib@gmail.com>
2025-08-27 15:58:11 +0600

    completed creating new warehouse

--- inventory-software/src/app/(back-office)/dashboard/inventory/warehouse/new/page.jsx
+++ inventory-software/src/app/(back-office)/dashboard/inventory/warehouse/new/page.jsx
@@ -1,5 +1,6 @@
 "use client"
 import FormHeader from '@/app/components/dashboard/FormHeader'
+import SelectInput from '@/app/components/FormInputs/SelectInput';
 import SubmitButton from '@/app/components/FormInputs/SubmitButton';
 import TextAreaInput from '@/app/components/FormInputs/TextAreaInput';
 import TextInput from '@/app/components/FormInputs/TextInput';
@@ -15,6 +16,17 @@ export default function NewWarehouse() {
     formState: { errors },
   } = useForm();
   const [loading, setLoading] = useState(false);
+
+  const selectOptions = [
+    {
+      label:"Main",
+      value: "main",
+    },
+    {
+      label:"Branch",
+      value: "branch",
+    },
+  ]
 
   async function onSubmit(data){
     try {
@@ -49,7 +61,11 @@ export default function NewWarehouse() {
         <div className='grid gap-4 sm:grid-cols-2 sm:gap-6'>
           <TextInput label="Warehouse Name" name="title" register={register} errors={errors} type="text" containerWidth="w-full"/>
 
-          <TextInput label="Warehouse Location" name="location" register={register} errors={errors} type="text" containerWidth="w-full"/>
+          <SelectInput name="type" label="Select the warehouse type" register={register} containerWidth='w-full' options={selectOptions}/>
+
+
+
+          <TextInput label="Warehouse Location" name="location" register={register} errors={errors} type="text" />
 
 
           {/* Description */}
@@ -58,7 +74,7 @@ export default function NewWarehouse() {
 
         </div>
 
-        <SubmitButton isLoading={loading} title="Category" />
+        <SubmitButton isLoading={loading} title="Warehouse" />
 
       </form>
 
